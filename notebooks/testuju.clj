1014b6ffdde9f16680f1e553dabe67dc098e5eaf
Tomáš Baránek <tombarys@users.noreply.github.com>
2025-05-20 12:39:59 +0300

    Zkrácena funkce pro převod dat na měsíce od vydání

--- notebooks/testuju.clj
+++ notebooks/testuju.clj
@@ -1,4 +1,6 @@
-(ns testuju)
+(ns testuju 
+  (:require
+    [scicloj.kindly.v4.kind :as kind]))
 (require '[scicloj.kindly.v4.kind :as kind]
          '[scicloj.metamorph.ml :as ml]
          '[java-time.api :as jt]
@@ -9,26 +11,21 @@
          '[tech.v3.dataset.column-filters :as ds-cf]
          '[tech.v3.dataset.modelling :as ds-mod]
          '[libpython-clj2.python :refer [py. py.-] :as py])
-      
 
-(def feed-string
-  (slurp "/Users/tomas/Downloads/melvil2.csv"))
+; ## Inicializace datasetu
+
+(defonce raw-ds (tc/dataset "/Users/tomas/Downloads/melvil2.csv" {:separator \tab}))
+
+
 
 (kind/hiccup
  [:div {:style {:max-height "400px"
                 :overflow-y :auto}}
-  (kind/code
-   feed-string)])
+  (kind/table
+   raw-ds)])
 
-(defn categorize-pages [page-count]
-  (cond
-    (< page-count 300) "normal"
-    (< page-count 450) "nadnormal"
-    :else "bichle"))
 
-(defonce raw-ds (tc/dataset "/Users/tomas/Downloads/melvil2.csv" {:separator \tab}))
-
-(def ds (ds/column raw-ds "Pocet_stran" ))
+; ## Pomocné funkce pro kategorizaci
 
 (defn cat-tloustka [ds]
   (map #(cond
@@ -43,13 +40,8 @@
 ;; filepath: /Users/tomas/Dev/noj-v2-getting-started/notebooks/testuju.clj
 ;; filepath: /Users/tomas/Dev/noj-v2-getting-started/notebooks/testuju.clj
 (defn months-on-market [date]
-  (when (some? date)
-    (let [start (if (instance? java.time.LocalDate date)
-                  date
-                  (try (jt/local-date "yyyy-MM-dd" date)
-                       (catch Exception _ nil)))
-          days (when start (jt/time-between start end-date :days))]
-      (when days (long (Math/round (/ days 30.4375)))))))
+  (when-let [days (when date (jt/time-between date end-date :days))]
+    (long (Math/round (/ days 30.4375)))))
 
 (def ds+kategorie
   (-> raw-ds
