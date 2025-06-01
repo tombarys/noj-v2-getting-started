2286eddb853f0c3c66ef4c1dd2816d1cc21d465b
Tomáš Baránek <tombarys@users.noreply.github.com>
2025-06-01 21:35:28 +0200

    Nefunguje

--- notebooks/libpythontest2.clj
+++ notebooks/libpythontest2.clj
@@ -8,14 +8,28 @@
          '[tech.v3.dataset :as ds]
          '[tech.v3.dataset.column-filters :as ds-cf]
          '[tech.v3.dataset.modelling :as ds-mod]
-         '[libpython-clj2.python :refer [py. py.-] :as py])
+         '[libpython-clj2.python :as py]
+         '[libpython-clj2.require :refer [require-python]])
 
-;; Initialize Python with the correct path (only once!)
-(py/initialize! :python-executable "/opt/homebrew/bin/python3")
+(py/initialize! :python-executable "/Users/tomas/Dev/noj-v2-getting-started/.venv/bin/python")
+
+(py/run-simple-string "import sys") 
+
+(require-python '[pandas :as pd])
+                '[numpy :as np]
+                '[sklearn.ensemble :as ensemble]
+                '[sklearn.model_selection :as model-selection])
 
 ;; Import sklearn modules after initialization
 (def sklearn-ensemble (py/import-module "sklearn.ensemble"))
 (def sklearn-model-selection (py/import-module "sklearn.model_selection"))
+
+;; Test pandas import
+(def pandas (libpython-clj2.python/import-module "pandas"))
+(def numpy (py/import-module "numpy"))
+
+;; Verify it works
+(py/py. pandas "DataFrame" {"a" [1 2 3] "b" [4 5 6]})
 
 (def iris
   (-> (toydata/iris-ds)
